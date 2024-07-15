c7823907ea6e9bb77861f81c6cd47635088d8643
unknown <min.ximenes@gmail.com>
2024-07-15 11:31:44 +0800

    upgrade

---
+++ aliclient/httpservice.py
@@ -0,0 +1,37 @@
+from flask import Flask, Response, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "200, OK!"
+
+
+@app.errorhandler(Exception)
+def handle_error(error):
+    return jsonify({"error": str(error)}), 500
+
+
+@app.route("/init-log")
+def get_log():
+    try:
+        logfile = "/var/log/cloud-init-output.log"
+        with open(logfile, "r") as f:
+            content = f.read()
+            return Response(content, minetype="text/plain")
+    except Exception as e:
+        return handle_error(e)
+
+
+@app.route("/ca-download")
+def download_ca():
+    try:
+        cafile = "/vpn-certs/server-root-ca.pem"
+        return send_file(f"{cafile}", as_attachment=True)
+    except Exception as e:
+        return handle_error(e)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
